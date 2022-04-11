40c8fc03c92abf11274eac7cad0be0fc88ca9ab2
eeue56 <enalicho@gmail.com>
2022-04-11 22:21:04 +0200

    add sort/sortBy

--- src/List_kernel.ts
+++ src/List_kernel.ts
@@ -5,3 +5,15 @@ export function kernelLength(xs: any[]): number {
 export function kernelEmptyList<a>(): a[] {
     return [];
 }
+
+export function kernelSort<a>(xs: a[]): a[] {
+    const ys = [...xs];
+    ys.sort();
+    return ys;
+}
+
+export function kernelSortBy<a>(fn: (_0: a, _1: a) => number, xs: a[]): a[] {
+    const ys = [...xs];
+    ys.sort(fn);
+    return ys;
+}
