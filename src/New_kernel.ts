5c1df77c45e41b0da5bbd30c33b5c7b4fdbb2948
eeue56 <enalicho@gmail.com>
2022-05-09 23:39:50 +0200

    add a lookup for make new

--- src/New_kernel.ts
+++ src/New_kernel.ts
@@ -1,3 +1,15 @@
-export function makeNew(constructor: any, args: any[]) {
+export function makeNew(constructor: any, args: any[]): any {
+    return new constructor(...args);
+}
+
+export function makeNewByName(constructorName: any, args: any[]): any {
+    const constructor: any = (globalThis as any)[constructorName];
+
+    if (typeof constructor === "undefined") {
+        console.error(
+            `Failed to find constructor ${constructorName} in global namespace.`
+        );
+    }
+
     return new constructor(...args);
 }
