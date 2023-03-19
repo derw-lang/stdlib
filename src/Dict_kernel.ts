9e53ed7e158b5877a8eac81ce929b481bd041994
eeue56 <enalicho@gmail.com>
2023-03-19 12:28:00 +0100

    add insert to dicts

--- src/Dict_kernel.ts
+++ src/Dict_kernel.ts
@@ -1,50 +1,61 @@
 import { Just, Maybe, Nothing } from "./Maybe";
 
-export function kernelUpdate(
-    a: any,
-    fn: (maybeValue: Maybe<any>) => Maybe<any>,
-    dict: any
+export function kernelInsert<key, value>(
+  key: key,
+  value: value,
+  dict: any
 ): any {
-    let updated: Maybe<any> = Nothing({});
-    if (Object.keys(dict).indexOf(a) === -1) {
-        updated = fn(Nothing({}));
-    } else {
-        updated = fn(Just({ value: dict[a] }));
+  const newDict = { ...dict };
+  newDict[key] = value;
+
+  return newDict;
+}
+
+export function kernelUpdate(
+  a: any,
+  fn: (maybeValue: Maybe<any>) => Maybe<any>,
+  dict: any
+): any {
+  let updated: Maybe<any> = Nothing({});
+  if (Object.keys(dict).indexOf(a) === -1) {
+    updated = fn(Nothing({}));
+  } else {
+    updated = fn(Just({ value: dict[a] }));
+  }
+
+  if (updated.kind === "Nothing") {
+    const newDict: any = {};
+
+    for (const key of Object.keys(newDict)) {
+      if (key !== a) {
+        newDict[key] = dict[key];
+      }
     }
-
-    if (updated.kind === "Nothing") {
-        const newDict: any = {};
-
-        for (const key of Object.keys(newDict)) {
-            if (key !== a) {
-                newDict[key] = dict[key];
-            }
-        }
-        return newDict;
-    }
-
-    const newDict = { ...dict };
-    newDict[a] = updated.value;
-
     return newDict;
+  }
+
+  const newDict = { ...dict };
+  newDict[a] = updated.value;
+
+  return newDict;
 }
 
 export function kernelFromList(items: { key: any; value: any }[]): any {
-    const obj: any = {};
+  const obj: any = {};
 
-    for (const item of items) {
-        obj[item.key] = item.value;
-    }
+  for (const item of items) {
+    obj[item.key] = item.value;
+  }
 
-    return obj;
+  return obj;
 }
 
 export function kernelToList(dict: any): { key: any; value: any }[] {
-    const items = [];
+  const items = [];
 
-    for (const key of Object.keys(dict)) {
-        items.push({ key, value: dict[key] });
-    }
+  for (const key of Object.keys(dict)) {
+    items.push({ key, value: dict[key] });
+  }
 
-    return items;
+  return items;
 }
